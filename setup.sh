b07a4e3ceb04b1f162c769e970263a2d8926b1f3
Brian Joe <brianwjoe@gmail.com>
2013-07-08 12:02:07 -0700

    Update setup.sh
    fixed a path error with dotfiles.git

--- setup.sh
+++ setup.sh
@@ -34,7 +34,7 @@ fi
 if [ -d .emacs.d/ ]; then
     mv .emacs.d .emacs.d~
 fi
-git clone https://github.com/supaxjoe/dotfiles.git
+git clone https://github.com/brianwjoe/dotfiles.git
 ln -sb dotfiles/.screenrc .
 ln -sb dotfiles/.bash_profile .
 ln -sb dotfiles/.bashrc .
